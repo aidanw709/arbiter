8370368885d48e9baf9dbb8bb04048cd1eab84f2
aidanw709 <98amwhite@gmail.com>
2023-04-17 00:03:07 -0400

    CWE558

--- vuln_templates/CWE558.py
+++ vuln_templates/CWE558.py
@@ -1,14 +1,20 @@
 def apply_constraint(state, expr, init_val, **kwargs):
+    addr = state.solver.eval(expr, cast_to=int)
+    if state.project.loader.find_section_containing(addr) is not None:
+        # Force an unsat error
+        state.solver.add(expr==0)
     return
 
 
 def specify_sinks():
-    maps = {'getlogin': ['']}
+    maps = {'getlogin': ['n']}
     return maps
 
 
 def specify_sources():
-    checkpoints = {'fork': ''}
+    checkpoints = {'fork': 0,
+                    'Fork': 0,
+                    'vfork': 0}
     return checkpoints
 
 
